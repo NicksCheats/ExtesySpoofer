f56538ba7e14daa8741ca28adcca7272fc2644fb
  <supersayinjack@gmail.com>
2022-08-10 03:51:15 -0400

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly with the following set of attributes
+// is checked. To change the information associated with the assembly
+// change these attribute values.
+[assembly: AssemblyTitle("HWIDSpoofer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HWIDSpoofer")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting the ComVisible attribute to false types in this assembly
+// Makes COM components invisible. A type in this assembly
+// set the ComVisible attribute to true of that type if you need access.
+[assembly: ComVisible(false)]
+
+
+// If this project is made available to COM, the following GUID is for the ID of the type library
+[assembly: Guid("b95c1bfb-4393-42f5-a718-97a8ef6c1ed8")]
+
+// An assembly's version information consists of the following four values:
+// Main Release
+// Secondary Version
+// Build Number
+// Correction
+// You can specify all values ​​or to use the default Build and Revision Numbers
+// you can use '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
